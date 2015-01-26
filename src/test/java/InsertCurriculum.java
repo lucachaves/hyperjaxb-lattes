114a3b4cc74ecf3a48c86a439aaea468ada44702
Luiz Carlos Rodrigues Chaves <lucachaves@gmail.com>
2015-01-25 22:52:31 -0300

    check if id16 exist before save

--- src/test/java/InsertCurriculum.java
+++ src/test/java/InsertCurriculum.java
@@ -16,6 +16,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
@@ -35,31 +36,18 @@ import junit.framework.TestCase;
 public class InsertCurriculum {
 
 	private ObjectFactory objectFactory;
-
 	private EntityManagerFactory entityManagerFactory;
-
 	private JAXBContext context;
-	
-	private Connection c = null;
+	private Connection connectionLattesXML = null;
+	private Connection connectionLattesRDB = null;
+	private EntityManager loadManager;
+	private EntityManager saveManager;
 	
 	public InsertCurriculum() {
-		try {
-			Class.forName("org.postgresql.Driver");
-			this.c = DriverManager
-					.getConnection("jdbc:postgresql://10.0.1.7:5432/lattesdata",
-							"postgres", "postgres");
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
 	}
 
-	public void setUp() throws Exception {
-
+	private void setUp() throws Exception {
 		objectFactory = new ObjectFactory();
-
 		final Properties persistenceProperties = new Properties();
 		InputStream is = null;
 		try {
@@ -71,129 +59,139 @@ public class InsertCurriculum {
 				try {
 					is.close();
 				} catch (IOException ignored) {
-
 				}
 			}
 		}
 
 		entityManagerFactory = Persistence.createEntityManagerFactory(
 				"generated", persistenceProperties);
-
 		context = JAXBContext.newInstance("generated");
+		saveManager = entityManagerFactory
+				.createEntityManager();
+		loadManager = entityManagerFactory
+				.createEntityManager();
+		Class.forName("org.postgresql.Driver");
+		connectionLattesXML = DriverManager
+				.getConnection("jdbc:postgresql://10.0.1.7:5432/lattesdata",
+						"postgres", "postgres");
+		connectionLattesRDB = DriverManager
+				.getConnection("jdbc:postgresql://10.0.1.7:5432/curriculum",
+						"postgres", "postgres");
+	}
+	
+	private void tearDown() throws SQLException {
+		connectionLattesXML.close();
+		connectionLattesRDB.close();
+		loadManager.close();
+		saveManager.close();
 	}
 
-	public List<Integer> getXmlIds(){
-		List<Integer> xmls = new ArrayList<Integer>();
+	private HashMap<Integer, String> getXmlIds() throws SQLException{
+		HashMap<Integer, String> xmls = new HashMap<Integer, String>();
 		
-		try {
-			Statement stmt = this.c.createStatement();
-//			ResultSet rs = stmt.executeQuery( "SELECT curriculums.id FROM public.curriculums WHERE curriculums.degree ILIKE '%pos-doutorado%' LIMIT 300;" );
-			ResultSet rs = stmt.executeQuery( "SELECT curriculums.id FROM public.curriculums WHERE curriculums.degree ILIKE '%pos-doutorado%';" );
-			while ( rs.next() ) {
-	            int  xml = rs.getInt("id");
-	            xmls.add(xml);
-//		            System.out.println( "XML = " + xml );
-//		            System.out.println( xml.substring(0, 200) );
-	            System.out.println( " # " );
-	         }
-	         rs.close();
-	         stmt.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		Statement stmt = connectionLattesXML.createStatement();
+		ResultSet rs = stmt.executeQuery( "SELECT curriculums.id, curriculums.id16 FROM public.curriculums WHERE curriculums.degree ILIKE '%pos-doutorado%';" );
+		while ( rs.next() ) {
+            Integer id = rs.getInt("id");
+            String  id16 = rs.getString("id16");
+            xmls.put(id, id16);
+            System.out.print( " # " );
+         }
+         rs.close();
+         stmt.close();
         
 		return xmls;
 	}
 	
-	public String getXml(Integer id){
+	private String getXml(Integer id) throws SQLException{
 		String xml = null;
 		
-		try {
-			Statement stmt = this.c.createStatement();
-	        ResultSet rs = stmt.executeQuery( "SELECT curriculums.xml FROM public.curriculums WHERE id = "+id+";" );
-	        while ( rs.next() ) {
-	            xml = rs.getString("xml");
-	         }
-	         rs.close();
-	         stmt.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		Statement stmt = connectionLattesXML.createStatement();
+        ResultSet rs = stmt.executeQuery( "SELECT curriculums.xml FROM public.curriculums WHERE id = "+id+";" );
+        while ( rs.next() ) {
+            xml = rs.getString("xml");
+         }
+         rs.close();
+         stmt.close();
 		
 		return xml;
 	}
 	
-	@SuppressWarnings("unchecked")
-	public void testRoundtrip() throws JAXBException {
-
-		Unmarshaller unmarshaller = context.createUnmarshaller();
-		EntityManager loadManager = entityManagerFactory
-				.createEntityManager();
-		EntityManager saveManager = entityManagerFactory
-				.createEntityManager();
-
-		System.out.println("Running...");
+	private Boolean hasCurriculum(String id16) throws SQLException{
+		String id = "";
+	
+		Statement stmt = connectionLattesRDB.createStatement();
+        ResultSet rs = stmt.executeQuery( "SELECT curriculo_vitae_type.NUMEROIDENTIFICADOR FROM public.curriculo_vitae_type WHERE NUMEROIDENTIFICADOR = '"+id16+"';" );
+        while ( rs.next() ) {
+            id = rs.getString("NUMERO-IDENTIFICADOR");
+         }
+         rs.close();
+         stmt.close();
 		
+		return id == "" ? false : true ;
+	}
+	
+	private void load() throws SQLException {
+		Unmarshaller unmarshaller;
 		int countXml = 0;
+		String xml = null;
+		String id16 = null;
+		InputStream in = null;
+		Object object = null;
+		CurriculoVitaeType xmlObject = null;
+		HashMap<Integer, String> xmls = getXmlIds();
+		int xmlsSize = xmls.size();
+		float percentage;
 		
-		for(Integer id: getXmlIds()){
-			
-			String xml = getXml(id);
-			
-//			System.out.println(countXml++ + " ");
-//			System.out.println(xml);
-			System.out.println(countXml++ + xml);
-			
-//			final Object object = unmarshaller.unmarshal(new File("src/test/samples/curriculum.xml"));
-			
-			InputStream in = new ByteArrayInputStream(xml.getBytes(StandardCharsets.UTF_8));
-			Object object = unmarshaller.unmarshal(in);
-			
-			CurriculoVitaeType xmlObject = ((JAXBElement<CurriculoVitaeType>) object)
-					.getValue();
-
+		for(Integer id: xmls.keySet()){
+			percentage = (++countXml * 100)/xmlsSize;
+			id16 = xmls.get(id);
+			if(hasCurriculum(id16))
+				continue;
+			 xml = getXml(id);
+			System.out.println(countXml +"/"+ xmlsSize +" "+ percentage +"% - "+ id16);
+			try {
+				unmarshaller = context.createUnmarshaller();
+	//			final Object object = unmarshaller.unmarshal(new File("src/test/samples/curriculum.xml"));
+				in = new ByteArrayInputStream(xml.getBytes(StandardCharsets.UTF_8));
+				object = unmarshaller.unmarshal(in);
+				xmlObject = ((JAXBElement<CurriculoVitaeType>) object).getValue();
+			} catch (JAXBException e) {
+				e.printStackTrace();
+			}
 			saveManager.getTransaction().begin();
+			if(xmlObject.getNUMEROIDENTIFICADOR() == null || xmlObject.getNUMEROIDENTIFICADOR().equals(""))
+				xmlObject.setNUMEROIDENTIFICADOR(id16);
 			saveManager.persist(xmlObject);
 			saveManager.getTransaction().commit();
-			
-//			Long id = alpha.getHjid();
-//
-//			CurriculoVitaeType c = loadManager.find(
-//					CurriculoVitaeType.class, id);
-//			CurriculoVitaeType c = loadManager.find(
-//					CurriculoVitaeType.class, (long) 1);
-//			CurriculoVitaeType c = loadManager.find(
-//					CurriculoVitaeType.class, new Long(1));
-			
-//			Marshaller marshaller = context.createMarshaller();
-//			marshaller.marshal(objectFactory.createCURRICULOVITAE(c), System.out);
 		}
-		
-		loadManager.close();
-		saveManager.close();
 	}
 	
 	
 	public static void main(String[] args) {
 		System.out.println("Iniciando o app");
+		
 		Date start = new Date();
+		
 		InsertCurriculum curriculum = new InsertCurriculum();
 		try {
 			curriculum.setUp();
-			curriculum.testRoundtrip();
-//			c.close();
+			curriculum.load();
+			curriculum.tearDown();
 		} catch (Exception e) {
 			System.out.println("Error!!!!!!");
 			e.printStackTrace();
+			System.exit(0);
 		}
-		Date end = new Date();
 		
-		long diff = end.getTime() - start.getTime();//as given
-
+		Date end = new Date();
+		long diff = end.getTime() - start.getTime();
 		long seconds = TimeUnit.MILLISECONDS.toSeconds(diff)%60;
 		long minutes = TimeUnit.MILLISECONDS.toMinutes(diff)%60;
 		long hours = TimeUnit.MILLISECONDS.toHours(diff);
 		
 		System.out.println("Finalizando o app em: "+hours+":"+minutes+":"+seconds);
 	}
+
+	
 }
